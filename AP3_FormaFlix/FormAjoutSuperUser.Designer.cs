bd9789a7372d88102723bb667c312c5d76ab334e
MataRed-dev <matared-dev@github.com>
2021-11-19 13:41:01 +0100

    V2.3.2 : Modification du formulaire pour ajotuer un utilisateur

--- AP3_FormaFlix/FormAjoutSuperUser.Designer.cs
+++ AP3_FormaFlix/FormAjoutSuperUser.Designer.cs
@@ -121,11 +121,11 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Bebas Neue", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(278, 107);
+            this.label1.Location = new System.Drawing.Point(178, 107);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 23);
+            this.label1.Size = new System.Drawing.Size(136, 23);
             this.label1.TabIndex = 14;
-            this.label1.Text = "Nom";
+            this.label1.Text = "Nom ( Id Connexion )";
             // 
             // label4
             // 

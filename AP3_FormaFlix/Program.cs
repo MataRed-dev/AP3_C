767bc3a104de6d158255a7a323d6156ed060b991
MataRed-dev <matared-dev@github.com>
2021-11-19 13:15:57 +0100

    Test concluant mais très difficile à mettre en page

--- AP3_FormaFlix/Program.cs
+++ AP3_FormaFlix/Program.cs
@@ -18,34 +18,39 @@ namespace AP3_FormaFlix
         /// Point d'entrée principal de l'application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void MainA()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormConnexion());
         }
 
-        static void MainA(string[] args)
+        static void Main(string[] args)
         {
             // Must have write permissions to the path folder
             PdfWriter writer = new PdfWriter("C:\\Users\\gouda\\OneDrive\\Documents\\AP3\\AP3_C\\demo.pdf");
             PdfDocument pdf = new PdfDocument(writer);
             Document document = new Document(pdf);
-            Paragraph header = new Paragraph("CERTICAT DE SUIVI DE FORMATION")
+            Paragraph header = new Paragraph("FORMAFLIX")
                .SetTextAlignment(TextAlignment.CENTER)
-               .SetFontSize(20);
-
-            document.Add(header);
-            Paragraph subheader = new Paragraph("Certification provenant de FORMAFLIX")
+               .SetFontSize(14);
+            LineSeparator ls = new LineSeparator(lineDrawer: new SolidLine());
+            Paragraph subheader = new Paragraph("CERTIFICAT DE SUIVI DE FORMATION")
                 .SetTextAlignment(TextAlignment.CENTER)
-                .SetFontSize(15);
+                .SetFontSize(25);
             Paragraph presentation = new Paragraph("Ce certificat a été délivré par l'entreprise BG'S COORP")
                 .SetFontSize(10)
                 .SetTextAlignment(TextAlignment.LEFT);
-            LineSeparator ls = new LineSeparator(lineDrawer: new SolidLine());
+            Paragraph ligne1 = new Paragraph("Délivré à (nom et prenom personne)");
+            Paragraph ligne2 = new Paragraph("Conformément au règles établies sur le site WEB FormaFlix le 12 novembre 2021 par les responsables");
+
+
+            document.Add(header);
             document.Add(ls);
             document.Add(subheader);
             document.Add(presentation);
+            document.Add(ligne1);
+            document.Add(ligne2);
             document.Close();
         }
     }
